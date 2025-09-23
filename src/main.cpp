a7638197b9c791427218da0091ce13431a773c37
sam <samtao.coulet@gmail.com>
2025-09-23 16:46:46 +0200

    feat: wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,58 @@
-#include <Arduino.h>
+#include <WiFi.h>          // pour ESP32 (ou <ESP8266WiFi.h> pour ESP8266)
+#include <PubSubClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Remplace par ton SSID et mot de passe WiFi
+const char* ssid = "BTS_CIEL_IR_ESP32";
+const char* password = "btscieliresp32";
+
+// Adresse IP du serveur MQTT
+const char* mqtt_server = "192.168.3.73";  
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastReconnectAttempt = 0;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // handle message arrived
+}
+
+boolean reconnect() {
+  if (client.connect("espClient")) {
+    // Une fois connecté, on publie un message
+    client.publish("esp32/bme280/temperature", "32");
+    // ... et on s'abonne au topic
+    client.subscribe("inTopic");
+  }
+  return client.connected();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connexion au WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connecté au WiFi");
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  lastReconnectAttempt = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (!client.connected()) {
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+    client.loop();
+  }
 }
