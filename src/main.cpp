519bbeaeb8f485ea1f398f32417866b6881104aa
sam <samtao.coulet@gmail.com>
2025-09-24 16:43:01 +0200

    feat: task

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@ const char* mqtt_password = "pwd_MQTT";   // mot de passe MQTT
 WiFiClient espClient;  // sert à crée un connecteur TCP
 PubSubClient client(espClient); //crée un client MQTT
 
-long lastReconnectAttempt = 0; // compteur pour la reconnexion
 
 void callback(char* topic, byte* payload, unsigned int length) { //sert à traiter les messages reçus
   // handle message arrived
@@ -25,8 +24,6 @@ void callback(char* topic, byte* payload, unsigned int length) { //sert à trait
 
 
 //_______________Test MQTT________________//
-int temperature = 0; // Variable pour stocker la température
-
 bool reconnect() { // Fonction de reconnexion au broker MQTT
   // Tentative de reconnexion
   if (client.connect("ESP32Client", mqtt_user, mqtt_password)) {
@@ -38,6 +35,20 @@ bool reconnect() { // Fonction de reconnexion au broker MQTT
 
 //--------------Fin test MQTT----------------//
 
+
+//_______________Variables globales________________//
+int temperature = 0; // Variable pour stocker la température
+long lastReconnectAttempt = 0; // compteur pour la reconnexion
+
+
+//--------------Fin variables globales----------------//
+
+//_______________Prototypes des fonctions________________//
+void connection_maintained(void *pvParameters);
+void sensorSimulation(void *pvParameters);
+void publishTemperature(void *pvParameters);
+
+//--------------Fin prototypes des fonctions----------------//
 
 
 void setup() {
@@ -64,33 +75,48 @@ void setup() {
 
   //--------------Fin partie MQTT----------------//
 
+	
+  xTaskCreate( connection_maintained, "Task MQTT", 10*configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+
+	xTaskCreate( sensorSimulation, "Task Sensor", 10*configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+
+  xTaskCreate( publishTemperature, "Task Publish", 10*configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+
+
 }
 
 //_______________reconnection________________//
-void connection_no_work(){
+void connection_maintained(__attribute__((unused)) void *pvParameters) {
+  for (;;) {
     if (!client.connected()) {
-    long now = millis();
-    if (now - lastReconnectAttempt > 5000) {
-      lastReconnectAttempt = now;
-      if (reconnect()) {
-        lastReconnectAttempt = 0;
+      long now = millis();
+      if (now - lastReconnectAttempt > 5000) {
+        lastReconnectAttempt = now;
+        if (reconnect()) {
+          lastReconnectAttempt = 0;
+        }
       }
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+    } else {
+      client.loop();
+      vTaskDelay(1000 / portTICK_PERIOD_MS); // partage du CPU
     }
-  } else {
-    client.loop();
   }
 }
-//___________________________________________//
+//--------------Fin reconnection----------------//
 
 
 //_______________émulaation capteur________________//
-void simulerCapteur() {
-  if (temperature < 50) {
-    temperature++;
-    Serial.print("Température: ");
-    Serial.println(temperature);
-  } else {
-    temperature = 0;
+void sensorSimulation(__attribute__((unused)) void *pvParameters ) {
+  for (;;) {
+    if (temperature < 50) {
+      temperature++;
+      Serial.print("Température: ");
+      Serial.println(temperature);
+    } else {
+      temperature = 0;
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Attend 1 seconde avant la prochaine incrémentation
   }
 }
 
@@ -98,9 +124,13 @@ void simulerCapteur() {
 
 
 //_______________envoi température________________//
-void envoyerTemperature() {
-  client.publish("esp32/bme280/temperature", String(temperature).c_str());
+void publishTemperature(__attribute__((unused)) void *pvParameters) {
+  for (;;) {
+    client.publish("esp32/bme280/temperature", String(temperature).c_str());
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // toutes les 2 secondes
+  }
 }
+
 
 //--------------Fin envoi température----------------//
 
